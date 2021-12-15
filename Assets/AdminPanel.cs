f53b5d754016d8d54edeef6f59d374a599b633bf
colin_schultz <schultz@majormega.com>
2021-12-15 09:35:47 -0500

    Init commit, unity 2020 LTS

---
+++ Assets/AdminPanel.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdminPanel : MonoBehaviour
+{
+    public void Restart()
+    {
+    }
+
+    public void Shutdown()
+    {
+
+    }
+
+    public void UpdateVolume(float volume)
+    {
+
+    }
+}
