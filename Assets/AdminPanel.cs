52946adda30aa5f88820ba2903b734959bc20040
Colin Schultz <schultz@majormega.com>
2021-12-15 11:16:54 -0500

    batch script running

--- Assets/AdminPanel.cs
+++ Assets/AdminPanel.cs
@@ -4,17 +4,32 @@ using UnityEngine;
 
 public class AdminPanel : MonoBehaviour
 {
-    public void Restart()
+    private const string restartComputerFilename = "restart_computers.bat";
+    private const string shutdownComputerFilename = "shutdown_computers.bat";
+
+    public void RestartComputers()
     {
+        RunBatchFile("test.bat");        
     }
 
     public void Shutdown()
     {
-
+        RunBatchFile("test.bat");
     }
 
     public void UpdateVolume(float volume)
     {
+    }
 
+    void RunBatchFile(string filename)
+    {
+        System.Diagnostics.ProcessStartInfo processInfo = new System.Diagnostics.ProcessStartInfo();
+        processInfo.UseShellExecute = false;
+        processInfo.RedirectStandardOutput = true;
+        processInfo.CreateNoWindow = true;
+        processInfo.FileName = Application.dataPath + "\\" + filename;
+        processInfo.WorkingDirectory = Application.dataPath;
+
+        System.Diagnostics.Process process = System.Diagnostics.Process.Start(processInfo);
     }
 }
